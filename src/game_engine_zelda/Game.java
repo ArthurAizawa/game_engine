363c237b912c07d94e88cef43097aecdd1913609
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2026-01-10 16:06:53 -0300

    Create game window with JFrame

--- src/game_engine_zelda/Game.java
+++ src/game_engine_zelda/Game.java
@@ -8,14 +8,20 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable {
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	
 	public final int WIDTH = 160;
 	public final int HEIGHT = 120;
 	public final int SCALE = 3;
 	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		frame = new JFrame();	
-
+		initFrame();	
+	}
+	
+	public void initFrame() {
+		frame = new JFrame("Game Ingine");
 		frame.add(this);
 		frame.setResizable(false);
 		frame.pack();
@@ -24,16 +30,54 @@ public class Game extends Canvas implements Runnable {
 		frame.setVisible(true);
 	}
 	
-	public void initFrame() {
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		
 	}
 	
 	public static void main(String args[]) {
 		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		long lasTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lasTime) / ns;
+			lasTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		} // fim while
 		
 	}
 
