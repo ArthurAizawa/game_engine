1bbe693f6ce9a70e8f04366d1b9e34529a1a5a9f
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2026-01-27 16:11:57 -0300

    Implementing metod clamp on the Camera

--- src/game_engine_zelda/entities/Player.java
+++ src/game_engine_zelda/entities/Player.java
@@ -77,8 +77,8 @@ public class Player extends Entity {
 			}
 		}
 		
-		Camera.x = this.getX() - (Game.WIDTH / 2);
-		Camera.y = this.getY() - (Game.HEIGHT / 2);
+		Camera.x =  Camera.clamp(this.getX() - (Game.WIDTH / 2),0, World.WIDTH*17 - Game.WIDTH);
+		Camera.y =  Camera.clamp(this.getY() - (Game.HEIGHT / 2),0, World.HEIGHT*16 - Game.HEIGHT);
 
 	
 	}
